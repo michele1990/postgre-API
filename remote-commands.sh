cda5486bcb82b5a5fdfa20fd434e3cec6a5b4a0c
northwestwind <michele.maestrale90@gmail.com>
2023-01-08 01:19:48 +0100

    test

---
+++ remote-commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /var/lib/postgresql/postgre-API
+echo "Running command: cd /var/lib/postgresql/postgre-API"
+eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-K04swRUnBoKd/agent.10604; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=10605; export SSH_AGENT_PID;
+echo Agent pid 10605;"
+echo "Running command:  eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-TXZymUa8hsZO/agent.10606; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=10607; export SSH_AGENT_PID;
+echo Agent pid 10607;" "
+ssh-agent bash
+
+ssh-add ~/.ssh/id_rsa
+Pastaefagioli1!
+echo "Running command:  ssh-add ~/.ssh/id_rsa "
+
+
+
+
+git --work-tree=/var/lib/postgresql/postgre-API --git-dir=/var/lib/postgresql/postgre-API/.git pull
+      
+echo "Running command: git --work-tree=/var/lib/postgresql/postgre-API --git-dir=/var/lib/postgresql/postgre-API/.git pull origin main
+      "
