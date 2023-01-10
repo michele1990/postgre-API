f095a370234703464879b38d62e0638507ccbe44
northwestwind <michele.maestrale90@gmail.com>
2023-01-10 22:50:19 +0100

    post second test

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.get('/', (request, response) => {
 
 app.get('/books', db.getUsers)
 app.get('/books/:author', db.getUserById)
-
 app.post('/bookpost',db. post);
-app.listen(3000, () => {
+
+app.listen(port, () => {
   console.log('Server running on port 3001')
 });
 
--- remote-commands.sh
+++ remote-commands.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /var/lib/postgresql/postgre-API
 echo "Running command: cd /var/lib/postgresql/postgre-API"
-eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-MpkhwyAhD9bp/agent.35838; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=35839; export SSH_AGENT_PID;
-echo Agent pid 35839;"
-echo "Running command:  eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-5FPBSqioQkm5/agent.35840; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=35841; export SSH_AGENT_PID;
-echo Agent pid 35841;" "
+eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-vEe6s4jSpwVK/agent.35944; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=35945; export SSH_AGENT_PID;
+echo Agent pid 35945;"
+echo "Running command:  eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-NM0BfOKmQR5L/agent.35946; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=35947; export SSH_AGENT_PID;
+echo Agent pid 35947;" "
 ssh-agent bash
 
 ssh-add ~/.ssh/id_rsa
