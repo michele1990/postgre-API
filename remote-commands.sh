cd45b7fd8c007dbe7ff7b2b35ad13461ee47d06d
northwestwind <michele.maestrale90@gmail.com>
2023-01-11 13:43:38 +0100

    pm2

--- remote-commands.sh
+++ remote-commands.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /var/lib/postgresql/postgre-API
 echo "Running command: cd /var/lib/postgresql/postgre-API"
-eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-BepoX34A8gZb/agent.38642; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=38643; export SSH_AGENT_PID;
-echo Agent pid 38643;"
-echo "Running command:  eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-oXcDWSEsvTsK/agent.38644; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=38645; export SSH_AGENT_PID;
-echo Agent pid 38645;" "
+eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-hImBfJ53Vfif/agent.38783; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=38784; export SSH_AGENT_PID;
+echo Agent pid 38784;"
+echo "Running command:  eval "SSH_AUTH_SOCK=/var/folders/7g/dy4nnfgn2694wvjkdm2473_r0000gn/T//ssh-ajiENMqeu1En/agent.38785; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=38786; export SSH_AGENT_PID;
+echo Agent pid 38786;" "
 ssh-agent bash
 
 ssh-add ~/.ssh/id_rsa
